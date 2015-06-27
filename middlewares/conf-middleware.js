23c25f379bca54518b46e0dc74241fa1ff34c0d9
Raabb Ajam <raabbajam@gmail.com>
2015-06-27 20:32:12 +0700

    Added middlewares

---
+++ middlewares/conf-middleware.js
@@ -0,0 +1,14 @@
+var conf = require('./conf');
+
+function confMiddleware(req, res, next) {
+  conf()
+    .then(function (json) {
+      req.app.locals.conf = json;
+      req.app.locals.ucfirst = function(value){
+          return value.charAt(0).toUpperCase() + value.slice(1);
+      };
+      return next();
+    });
+}
+
+module.exports = confMiddleware;
